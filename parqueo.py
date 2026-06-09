0b8e3b8e0505b345c2511b48f74aac829d38d5d9
ah23007CesiaAlfaro <ah23007@ues.edu.sv>
2026-06-09 11:50:36 -0600

    funcion de lista enlazada

--- parqueo.py
+++ parqueo.py
@@ -42,3 +42,35 @@ class PilaHistorial:
     def mostrar(self):
         if not self.historial: return "Sin acciones recientes."
         return "\n".join([f" | {act.upper()}: {v.placa} en {z}" for act, v, z in reversed(self.historial)])
+    
+
+# =====================================================================
+# 3. LISTA ENLAZADA - Registro permanente de usuarios
+# =====================================================================
+class NodoLista:
+    def __init__(self, vehiculo):
+        self.vehiculo = vehiculo
+        self.siguiente = None
+
+class ListaRegistrados:
+    def __init__(self):
+        self.cabeza = None
+
+    def registrar(self, vehiculo):
+        nuevo = NodoLista(vehiculo)
+        nuevo.siguiente = self.cabeza
+        self.cabeza = nuevo
+
+    # RECURSIVIDAD en la busqueda de la Lista Enlazada
+    def buscar_recursivo(self, nodo, placa):
+        if nodo is None: return None
+        if nodo.vehiculo.placa == placa.upper(): return nodo.vehiculo
+        return self.buscar_recursivo(nodo.siguiente, placa)
+
+    def mostrar(self):
+        if not self.cabeza: return "Ninguno"
+        res, actual = [], self.cabeza
+        while actual:
+            res.append(f"[{actual.vehiculo.placa}: {actual.vehiculo.propietario}]")
+            actual = actual.siguiente
+        return " -> ".join(res) + " -> None"
