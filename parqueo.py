e1ec5cba44679b279afd4750106868d898849280
Your Name <you@example.com>
2026-06-09 10:40:53 -0600

    Commit 2: Agregar clase ColaEspera

--- parqueo.py
+++ parqueo.py
@@ -8,3 +8,20 @@ class Vehiculo:
         self.placa = placa.upper()
         self.propietario = propietario
         self.tipo = tipo.capitalize() # Sedan, Motocicleta, Camion
+
+# =====================================================================
+# 1. COLA (FIFO) - Espera de ingreso
+# =====================================================================
+class ColaEspera:
+    def __init__(self):
+        self.items = deque()
+
+    def encolar(self, vehiculo):
+        self.items.append(vehiculo)
+
+    def desencolar(self):
+        return self.items.popleft() if self.items else None
+    
+    def mostrar(self):
+        if not self.items: return "Vacia"
+        return " <- ".join([f"[{v.placa}]" for v in self.items])
